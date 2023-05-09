c9ebd23d656b89388fc0c5e7efc90c337b2c40bf
Yopa Nelly <121314923+YopaNelly@users.noreply.github.com>
2023-05-09 19:32:16 +0100

    Update Ex26.py

--- Ex26/Ex26.py
+++ Ex26/Ex26.py
@@ -1,5 +1,4 @@
-# Create a function that will receive two arrays and will return an array with elements that are in the first array
-# but not in the second
+# Create a function that will receive two arrays and will return an array with elements that are in the first array but not in the second
 first_array = [1, 2, 3, 10, 5, 3, 14]
 second_array = [-1, 4, 5, 6, 14]
 
